caa652cc3e22f68c8bd2cc3346eb3f872eac46fe
jfsas <1540253153@qq.com>
2025-05-06 20:10:19 +0800

    feat: update

--- iot/__init__.py
+++
@@ -1,5 +0,0 @@
-from iot.terminal import Terminal
-from iot.factories import RecorderFactory, PlayerFactory, AsrProcessorFactory, TtsProcessorFactory
-from iot.hooks import HooksGroup
-from iot.semantics import SemanticsGroup
-from iot.models import *
